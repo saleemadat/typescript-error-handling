3ed6210814ee440be0cc12899500e59f208c7bcb
saleem <saleem@nate.tech>
2021-10-03 11:57:20 +0100

    chore: empty infra

--- infrastructure/backend.hcl
+++
@@ -1,4 +0,0 @@
-bucket         = "nate-terraform-state-store"
-region         = "eu-west-1"
-dynamodb_table = "nate-terraform-state-locks"
-encrypt        = true
--- infrastructure/repository/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ecr_repository_repository_url" {
-  value = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/nate-${var.aws_region}-${var.dominion}-${var.subdominion}-${var.version_name}-ecr"
-}
